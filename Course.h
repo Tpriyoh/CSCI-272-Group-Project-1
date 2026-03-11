3cfb899cb238ff7350e85fb99fb5dcc79bbff390
Tpriyoh <29980049+Tpriyoh@users.noreply.github.com>
2026-03-11 13:00:53 -0400

    Refactor comments and fix getGradePoints logic
    Updated comments for clarity and fixed minor issues in the Course.h file.

--- Course.h
+++ Course.h
@@ -34,7 +34,8 @@ Course() {
     } 
     
     //const getters - wont let others modify the classes data.
-    // the const at the end means the function wont affect anything else.
+    //using const at the end makes the function read only so you don't accidentally change your data
+    //it stops bugs and is a good habit or so i was told by Claud AI 
     string getName() const {
         return name;
     }
@@ -46,8 +47,9 @@ Course() {
     }
     
     // converting letter grades to points
-    //error note: didnt add grade == after ||->(or) makinng everything default to 4.0
+    //error note: didnt add grade == after ||  makinng everything default to 4.0
     //but I added the or just incase the user uses either a capital or lowercase letter.
+    // we later fixed this problem using <cctype> making all lowercase letters default to capital letters.
     double getGradePoints() const {
         if (grade == "A" || grade == "a") return 4.0;
         if (grade == "A-" || grade == "a-") return 3.7;
@@ -61,11 +63,16 @@ Course() {
         if (grade == "F" || grade == "f") return 0.0;
         return 0.0; //if none of the above match.
     }
+    
+    //needed for student.h to calculate total points.
     double getTotalPoints() const {
         return credits * getGradePoints();
     }
     
+    
     //cleaner display function for all data on a formatted line.
+    //Refrence https://en.cppreference.com/w/cpp/io/manip/setw
+    //had issues where if the setw size was too small letters would output without any spacing. like setw(5)
     void display() const {
         cout << left << setw(30) << name
         << right << setw(15) << credits
