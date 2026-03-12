d3fd93956ac24ec689706d29bbf034127cb586ba
Tpriyoh <29980049+Tpriyoh@users.noreply.github.com>
2026-03-11 20:54:13 -0400

    Add files via upload

---
+++ Group Project/Course.h
@@ -0,0 +1,96 @@
+/* 
+Name: Tanzin Priyoh
+Course: CSCI 272(1)
+Group Project 1
+*/
+
+
+#include <iostream>
+#include <string>
+#include <iomanip>
+using namespace std;
+
+
+// the course blueprint
+class Course {
+    //private data only accessable to the class
+private:
+    string name; //ex math, english, science etc...
+    int credits; //how many credits its worth.
+    string grade; //letter grades only.
+    
+public:
+// Default constructor useful for arrays:
+Course() {
+    name = "";
+    credits = 0;
+    grade = "";
+}
+//public data, built a constructor to make private data accessable we must identify the name,credits,grade in int main later.
+    Course(string n, int c, string g) {
+        name = n;
+        credits = c;
+        grade = g;
+    } 
+    
+    //const getters - wont let others modify the classes data.
+    //using const at the end makes the function read only so you don't accidentally change your data
+    //it stops bugs and is a good habit or so i was told by Claud AI 
+    string getName() const {
+        return name;
+    }
+    int getCredits() const {
+        return credits;
+    }
+    string getGrade() const {
+        return grade;
+    }
+    
+    // converting letter grades to points
+    //error note: didnt add grade == after ||  makinng everything default to 4.0
+    //but I added the or just incase the user uses either a capital or lowercase letter.
+    // we later fixed this problem using <cctype> making all lowercase letters default to capital letters.
+    // source of grade policy: https://www.cuny.edu/wp-content/uploads/sites/4/page-assets/about/administration/offices/registrar/resources/CUNY-Uniform-Grade-Glossary-and-Guidelines-08012018-1.pdf
+    double getGradePoints() const {
+        if (grade == "A" || grade == "a") return 4.0;
+        if (grade == "A-" || grade == "a-") return 3.7;
+        if (grade == "B+" || grade == "b+") return 3.3;
+        if (grade == "B" || grade == "b") return 3.0;
+        if (grade == "B-" || grade == "b-") return 2.7;
+        if (grade == "C+" || grade == "c+") return 2.3;
+        if (grade == "C" || grade == "c") return 2.0;
+        if (grade == "C-" || grade == "c-") return 1.7;
+        if (grade == "D" || grade == "d") return 1.0;
+        if (grade == "F" || grade == "f") return 0.0;
+        return 0.0; //if none of the above match.
+    }
+    
+    //needed for student.h to calculate total points.
+    double getTotalPoints() const {
+        return credits * getGradePoints();
+    }
+    
+    
+    //cleaner display function for all data on a formatted line.
+    //Refrence https://en.cppreference.com/w/cpp/io/manip/setw and lesson 5 PDF
+    //had issues where if the setw size was too small letters would output without any spacing. like setw(5)
+    void display() const {
+        cout << left << setw(30) << name
+        << right << setw(15) << credits
+        << setw(15) << grade << fixed << setprecision(2)
+        << setw(15) << getGradePoints()
+        << setw(15) << getTotalPoints() << endl;
+    }
+};
+
+/*
+Tanzin Priyoh --- 3/7/26
+The difficult parts for me were figuring out where const belonged in the getter functions
+because I kept getting errors until I figured out it belongs after you make the paramater list.
+For setw() I had a problem with numbers printing next to eachother because I used a small range like setw(5)
+instead of  giving it more room inbetween entries.
+Next I had a problem with the getGradePoints() function because I wrote (grade == "A" || "a") which made the entire list 
+default to 4.0 everytime you ran the code.
+And in int main() I used #include <cctype> to force all lowercase letters to become uppercase unlike here because I
+forgot about how to originally do that until I looked up a website to find a refrence for how it worked.
+*/
---
+++ Group Project/main.cpp
@@ -0,0 +1,153 @@
+/* Name: Tun Tun (Jonathan) Aung
+ * Course: CSCI 272(1)
+ * Professor: Avijit Roy
+ * Date: Spring 2026
+ * Team Project 1
+ * Member: Tanzin Priyoh, Tun Tun (jonathan) Aung, Miguel Llapa, Bilal Ahmed
+*/
+#include <limits> //cite: for purpose of .begin() .end(), introduced through claude, further learning done through stackoverflow post on syntax especially the idea of why != instead of ==.
+#include <algorithm>
+#include <cctype> //need this to force all letter grades to be capital letters // source: https://www.geeksforgeeks.org/cpp/convert-cpp-string-to-uppercase/ second example.
+#include "student.h"//Only need this file since my teammate have pretty much included everything else
+
+
+int main()  {
+    cout << string(62, '=') << "\n"; //format line
+    cout << setw(42) << "Student Grade Report\n";
+    cout << string(62, '=') << "\n"; //formatting
+
+
+    //vector<Course> courses; // a vector to hold all courses from the users entries.// we already have this in student.h dont need it here
+    string name(""), grade("");
+    int numOfCourses(0), credits(0), totalCredit(0); //made int and string vales = to 0 or blank to prevent autofill from occuring.
+
+
+
+    cout << "Student Name: :" ; //Prompt user to enter name
+    getline(cin, name);
+    Student student(name); //Create student object using teammate's student class. Will own all courses // Moved the line down so the student name actually prints on the report card since we are using the student.h file
+
+
+
+
+    cout << "How many courses did you take?(1-10): ";
+    //While loop was study and referenced through combination of stackoverflow and claude ai for logic understanding
+    while (!(cin >> numOfCourses)|| numOfCourses <1 || numOfCourses > 10) { //validate bad input, this says, keep asking for input until there is no empty input and its within 1 < x < 10
+        cin.clear(); //reset cin so it can be used again
+        cin.ignore(numeric_limits<streamsize>::max(), '\n'); //learned in class, trying this, prof says this flushes the bad input during class. basically after every cin >>, theres left over '\n', in buffer. this .ignore(...) helps us ignore it
+        cout <<" Invalid input. Enter a number between 1 and 10: ";
+    }
+    cin.ignore(); //clears leftover newline
+    
+    
+    
+    
+    for(int i = 0; i < numOfCourses; ++i) { //loop for every course mentioned.
+		//failsafes worked on by Johnathan 
+		//Letter grade portion worked on by Tanzin using geeksforgeeks as a refrence.
+
+        cout << "Course Name: ";
+        getline(cin, name);
+        
+        
+        
+
+        cout << "Credit Hours: ";
+        while (!(cin >> credits) || credits < 0) {
+            cin.clear();
+            cin.ignore(numeric_limits<streamsize>::max(), '\n'); //build a habit of using this after every cin.
+            cout << "Invalid input, put in number greater than 0: ";
+        }
+        totalCredit += credits;
+        cin.ignore(); //added again so getline works once more when i call it again.
+
+
+
+        
+        
+        cout << "Letter Grade(A, A-, B+, B, B-, C+, C, C-, D, F): ";
+		getline(cin, grade);
+
+        //Refrence: https://www.geeksforgeeks.org/cpp/convert-cpp-string-to-uppercase/ second example.
+		for(int j = 0; j < grade.size(); ++j) {
+			grade[j] = toupper(grade[j]);
+		}
+
+        //keeps checking if the grade isnt one of the following ones. if invalid it will say invalid entry.
+        //if grade DOES = "A" the loop stops right there and spits out that result
+        //if it equals nothing on that list it will push the error.
+        //Source: Claud AI: had to look up a different way to validate a correct entry because a simple if else statement created a problem where 
+        // "Letter grade..." would repeat infinitely.
+		while (grade != "A" && grade != "A-" && grade != "B+" && grade != "B" &&
+		       grade != "B-" && grade != "C+" && grade != "C" && grade != "C-" &&
+		       grade != "D" && grade != "F") {
+
+			cout << "Invalid Entry. What is your 'letter' grade(A, A-, B+, B, B-, C+, C, C-, D, F): ";
+			getline(cin, grade);
+			
+			//Refrence: https://www.geeksforgeeks.org/cpp/convert-cpp-string-to-uppercase/ second example.
+			for(int j = 0; j < grade.size(); ++j) {
+			grade[j] = toupper(grade[j]);
+		}
+		
+		}
+		
+		
+        
+        //once a course object is made its given data is added to this vector
+        //and pushed to the back of the list.
+
+        //courses.push_back(Course(name, credits, grade)); //Tanzin's code before student.h was added.
+		//Jonathan's Update
+        student.addCourse(Course(name, credits, grade));
+
+        cout << endl; //added so theres a clear space when a new courses info is being requested.
+    } //end of loop.
+    
+    
+    
+    cout << "\n\n";
+    
+    
+
+    // loops through all courses and shows them all in a single line format.
+    cout << left  << setw(30) << "Course"
+         << right << setw(15) << "Credits"
+                  << setw(15) << "Grade"
+                  << setw(15) << "Grade Points"
+                  << setw(15) << "Total Points" << endl;
+
+        cout << string (90, '-') << "\n" ; //formatting line
+    student.displayReportCard();
+
+
+    }
+
+
+// I (Bilal) Worked on fixing the errors and made the extra credit sorting algorithum.
+// Id say the hard part for me was the intitial errors that I into where the code wasnt working due to getCourse and getGrade function not being called properly untill i deleted them and the function was running without a problem.
+// There were some extremely minor errors that were causing issue aswell. It took me a while to realise i can just use the ctrl+f command to make sure were calling the right function.
+// The final issue that I had was with sorting where I first tried for loops but they were kind of confusing. I watched a video on youtube where it showed me asimple sort library that pretty much does it for me.
+
+
+
+/* Tun Tun (Jonathan) Aung -- 3/7/26
+ * Reflection and challenges:
+ * 1. WIthin this group, my role is to take care of the main.cpp, which
+ * poses few challenges such as working with what others have done and making sure that I am
+ * recognizing public and private data and understanding how to format them.
+ * Additionally, everyone worked at a different pace so there are also diffculties trying to move
+ * forward without having other member done their part. Alot of place holders were used at _wfindfirstbut
+ * since its diffcult to know how other members will work on their code.
+ * E.g. I wasn't sure if I am responsible to calculate the gpa and standing in the main or
+ * if member from student class is going to be working with it. (Avoiding redundancy)
+ *
+ * 2. Trying to come up with ways to use while loop to error proof each input intake and their scenarios.
+ *
+ * 3. Understanding the use of cin.clear() and cin.ignore(numeric_limits<streamsize>::max(), '\n') as this is my firs
+ * time using this.
+ *
+ * 4.While loop was not covered in class but the concept was introduced to me before through prior classes, however syntax and use were different
+ * i used resoures like claude ai and stackoverflow to understand. AI to provide detail break down examples, and stackoverflow to see realworld
+ * nuance.
+ * */
